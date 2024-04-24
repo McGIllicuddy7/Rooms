226e099f6f489506321cd98b3256d0323b3758f7
Bridget Hartin Pasco <isaacpasco@icloud.com>
2024-04-23 22:21:14 -0700

    bruh

--- src/config.rs
+++ src/config.rs
@@ -1,6 +1,6 @@
 pub const SCREEN_HEIGHT:i32 = 1000;
 pub const SCREEN_WIDTH:i32 = 1000;
-pub const MIN_AREA:i32 = 16*16*2;
+pub const MIN_AREA:i32 = 20*20*2;
 pub const BUILDING_MIN:i32= 200;
 pub const BUILDING_MAX:i32 = 600;
 pub const DEBUG_TIMING:bool = true;
